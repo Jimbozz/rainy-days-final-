327d8514ae7dc446bb884d73660ca37dc507f614
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2021-10-27 15:08:09 +0200

    updated featured

--- js/featured.js
+++ js/featured.js
@@ -2,17 +2,15 @@ const featured = document.querySelector(".card-container");
 const link = "http://localhost/rainy-days-v2/index.php/wp-json/wc/store/products?featured=true";
 const bodyTag = document.querySelector(".run-scripts");
 
-//Need to remove this from women and mens product pages(Need to set class tag for each body tag I want it to run on)
+
 async function getFeatured() {
 
   try {
      const response = await fetch(link);
      const getResults = await response.json();
-     console.log(getResults);
 
     for(let i = 0; i < getResults.length; i++) {
       
-
       featured.innerHTML += `
       <a href="/product-specific.html?id=${getResults[i].id}">
         <div class="item one">
--- js/women.js
+++ js/women.js
@@ -1,4 +1,5 @@
 const url = "http://localhost/rainy-days-v2/index.php/wp-json/wc/store/products?exclude=17,19,21";
+
 const productContainer = document.querySelector(".card-container");
 
 async function getProducts() {
@@ -6,10 +7,11 @@ async function getProducts() {
   try {
      const response = await fetch(url);
      const getResults = await response.json();
-    //  createHTML(getResults);
      console.log(getResults);
+     
 
     for(let i = 0; i < getResults.length; i++) {
+      console.log(getResults[i].prices.price);
 
       productContainer.innerHTML += `
       <a href="/product-specific.html?id=${getResults[i].id}">
@@ -20,6 +22,24 @@ async function getProducts() {
         </div>
       </a>
       `;
+
+
+      var checkboxLow = document.querySelector('input[name=price-low]');
+      var checkboxHigh = document.querySelector('input[name=price-high]');
+    
+    
+    
+    [document.querySelector('input[name=price-low]'), document.querySelector('input[name=price-high]')].forEach(checkboxes => {
+      checkboxes.addEventListener('change', function(event) {
+        if (checkboxLow.checked) {
+          console.log("prices Low");
+          getResults.reverse();
+      } else if(checkboxHigh.checked) {
+        console.log("Prices High");
+      }
+      });
+    });
+      
     }
   }
   catch(error) {
@@ -30,16 +50,6 @@ async function getProducts() {
 getProducts();
 
 
-// function createHTML(products) {
-//   products.forEach(function(product) {
-//     productContainer.innerHTML += `
-//     <a href="/product-specific.html?id=${}">
-//       <div class="item one">
-//         <img src="${product.images[0].src}" alt="${product.name}">
-//         <h3>${product.name}</h3>
-//         <p class="card-price">$${product.prices.price}</p>
-//       </div>
-//     </a>
-//     `;
-//   })
-// }
+
+
+
