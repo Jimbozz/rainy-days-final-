9b27137336757aa51be3698416ad2a88a7cc3a48
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2021-12-17 11:08:15 +0100

    updated titles and meta descriptions

--- js/productSpecific.js
+++ js/productSpecific.js
@@ -2,6 +2,7 @@ const url = "https://jamesbadenhorst.one/index.php/wp-json/wc/store/products";
 const productImage = document.querySelector(".left-wrapper");
 const box1 = document.querySelector(".box1");
 const box2 = document.querySelector(".box2");
+const title = document.querySelector("title");
 const queryString = document.location.search;
 const getNum = queryString.replace( /^\D+/g, '');
 const updatedUrl = url + "?include=" + getNum;
@@ -12,6 +13,8 @@ async function getProducts() {
   try {
      const response = await fetch(updatedUrl);
      const getResults = await response.json();
+     title.innerHTML = `Rainydays | ${getResults[0].name}`;
+     console.log(getResults);
      createHTML(getResults);
     
   }
